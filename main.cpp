b13d754920a551499fd36c94d746c86c6c7c802d
FAgrippino <fagrippino@itba.edu.ar>
2022-03-25 13:55:04 -0300

    Feat: Deteccion de Subcripcion y Conexion

--- main.cpp
+++ main.cpp
@@ -3,25 +3,26 @@
 #include <vector>
 
 int main (void){
-
     MQTTClient cliente1("robot1");
-
     cliente1.connect("127.0.0.1", 1883, "user", "vdivEMMN3SQWX2Ez");
-    int var1 = cliente1.isConnected();
-    if (!var1) {
+    if (!cliente1.isConnected()) {
         std::cout << "Connection Error" << "\n";
         return 1;
     }
-
-    cliente1.subscribe("robot1/motor[1]/voltage");
-    
+    if (cliente1.subscribe("[robot1]/motor[1]/voltage")) {
+        std::cout << "Subscription Error" << "\n";
+        return 1;
+    }
+    std::cout << "Connection & Subscription Successful" << "\n";
     while (1) {
         std::vector<MQTTMessage> mensaje = cliente1.getMessages();
-        for (unsigned int i=0; i < mensaje.size(); i++){
-            for (std::vector<char>::iterator j = mensaje[i].payload.begin(); j != mensaje[i].payload.end(); j++)
-            std::cout << (float)*j << ", ";       
+        if (!mensaje.empty()) {
+            for (unsigned int i = 0; i < mensaje.size(); i++) {
+                for (std::vector<char>::iterator j = mensaje[i].payload.begin(); j != mensaje[i].payload.end(); j++)
+                    std::cout << (float)*j << ", ";
+            }
+            std::cout << "\n";
         }
-        std::cout << "\n";
     }
     
 
